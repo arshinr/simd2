ea841946c59ccc6031d36997391271cc883e06cd
Arshin <arshinr@yahoo.com>
2021-06-08 11:26:27 -0400

    Update script0710cppmnew.sh

--- script0710cppmnew.sh
+++ script0710cppmnew.sh
@@ -44,7 +44,7 @@ do
 
 		mkdir $STNumber
 
-		java -Xmx30g -Dfile.encoding=UTF-8 -classpath bin:jars/cloudsim-3.0.3-sources.jar:jars/cloudsim-3.0.3.jar:jars/cloudsim-examples-3.0.3-sources.jar:jars/cloudsim-examples-3.0.3.jar:jars/commons-math3-3.5/commons-math3-3.5.jar:jars/guava-18.0.jar:jars/json-simple-1.1.1.jar:jars/junit.jar:jars/org.hamcrest.core_1.3.0.v201303031735.jar org.fog.vmmobile.AppExample 1 290538 0 0 $STNumber 11 3 0 0 61 $getPeriodicTimeUp $getPeriodicTimeDown 2439 $VMSizeMin $VMSizeMax
+		java -Xmx30g -Dfile.encoding=UTF-8 -classpath bin:jars/cloudsim-3.0.3-sources.jar:jars/cloudsim-3.0.3.jar:jars/cloudsim-examples-3.0.3-sources.jar:jars/cloudsim-examples-3.0.3.jar:jars/commons-math3-3.5/commons-math3-3.5.jar:jars/guava-18.0.jar:jars/json-simple-1.1.1.jar:jars/junit.jar:jars/org.hamcrest.core_1.3.0.v201303031735.jar org.fog.vmmobile.AppExample 1 290538 0 0 $STNumber 11 3 0 0 61 $getPeriodicTimeUp $getPeriodicTimeDown $VMSizeMin $VMSizeMax
 
 		mv *.txt $STNumber
 		mv $STNumber out/Pre/
